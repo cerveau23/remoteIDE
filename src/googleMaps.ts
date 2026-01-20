321b667ddb189d10a08e86db31d9cd96b06bad25
Cerveau23 <cerveau23@gmail.com>
2026-01-20 16:37:55 +0100

    Added automatic type determination for portReceiver in functions.ts, extracted all the custom types in a dir and wrapped the types declared in Geography.ts in a namespace.

--- src/googleMaps.ts
+++ src/googleMaps.ts
@@ -1,19 +1,25 @@
 // noinspection InfiniteLoopJS
 
 import { NS } from "@ns";
-import { dSe , Map, Location, PartialFlag} from "depthScannerV2";
-function scanner(ns : NS, arg?: PartialFlag) {
+import { dSe , PartialFlag} from "depthScannerV2";
+import {Location, Map} from "/typeLib/Geography";
+function scanner(ns : NS, arg?: PartialFlag) : Map {
 	return dSe(ns,arg);
 }
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
+	let scanMap;
+	let previousMap;
 	//@ignore-infinite
 	while (true) {
 		let portHandle = ns.getPortHandle(1);
 		while (!portHandle.full()) {
-			let scanMap = <Map>scanner(ns);
-			portHandle.tryWrite({ name: "Server Map", data: scanMap, loop: true });
+			scanMap = <Map>scanner(ns);
+			if(previousMap != scanMap) {
+				portHandle.tryWrite({name: "Server Map", data: scanMap, loop: true});
+				previousMap = scanMap;
+			}
 		}
 		await ns.sleep(10);
 	}
---
+++ src/typeLib/Geography.ts
@@ -0,0 +1,31 @@
+export namespace Geography{
+    /**
+     * Represents a location on the net.
+     * @property {Hostname} ServerName - The location's name.
+     * @property {Number} ServerID - The location's UID.
+     * @property {Path} Path - The path to the location.
+     * @property {Boolean} HasChildren - Whether the location has children locations.
+     * @property {Boolean} [Backdoored] - Whether the server has been backdoored in the past.
+     */
+    export type Location = [
+        /** Server Name */
+        ServerName: Hostname, // Test
+        /** Server ID */
+        ServerID: number,
+        /** Path to the Server via IDs */
+        Path: Path,
+        /** Whether the server has "children" */
+        HasChildren: boolean,
+        /** whether the server has been backdoored in the past */
+        Backdoored?: boolean
+    ]
+    export type Hostname = string;
+    /** An array of {@link Location locations}.*/
+    export type Map = Location[];
+
+    /**
+     * The path to a {@link ``Location``}, represented as an array of ``UIDs`` from ``Home`` to the ``Location``
+     */
+    export interface Path extends Array<number> {
+    }
+}
