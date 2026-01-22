ff85d1e54e232503948c947b273fc29aa0e31ff5
Cerveau23 <cerveau23@gmail.com>
2026-01-22 18:43:40 +0100

    Migrated start.js to TS

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,7 @@
+import { NS } from "@ns";
+
 /** @param {NS} ns */
-export async function main(ns) {
+export async function main(ns: NS) {
   while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 1000, ns.args[2]) !== 0) { await ns.sleep(1); }
   while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 100, ns.args[2]) !== 0) { await ns.sleep(1); }
   while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 10, ns.args[2]) !== 0) { await ns.sleep(1); }
