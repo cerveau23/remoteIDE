f9c33bce728dad59f9b9b71dd99e7cee52b1625b
Cerveau23 <cerveau23@gmail.com>
2026-01-22 16:34:31 +0100

    Migrated contractTester.js to TS

--- src/contractTester.ts
+++ src/contractTester.ts
@@ -1,8 +1,9 @@
+import { NS } from "@ns";
 import { copyToClipboard } from "semiAutoBackdoorer"
 /** @param {NS} ns */
-export async function main(ns) {
-	let type = await ns.prompt("Type:", { type: "select", choices: ns.codingcontract.getContractTypes() })
-	let massTest = await ns.prompt("Mass Testing?", { type: "boolean" })
+export async function main(ns: NS) {
+	let type = <string> await ns.prompt("Type:", { type: "select", choices: ns.codingcontract.getContractTypes() })
+	let massTest = <boolean> await ns.prompt("Mass Testing?", { type: "boolean" })
 	if (massTest) {
 		for (let i = 0; i < 1000; i++) {
 			ns.codingcontract.createDummyContract(type)
