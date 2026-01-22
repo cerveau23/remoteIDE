fac693053162ef39a92dc0883ce2b9d12a5a903d
Cerveau23 <cerveau23@gmail.com>
2026-01-22 16:46:31 +0100

    Migrated dataScruber.js to TS

--- src/dataScruber.ts
+++ src/dataScruber.ts
@@ -1,10 +1,11 @@
 //import dSe from "./depthScanner.js";
+import { NS } from "@ns";
 import { portReceiver } from "functions";
 /** @param {NS} ns */
-export async function main(ns) {
+export async function main(ns: NS) {
 	let authorisedFormats = ["txt", "lit"]
 	//let mapp = await dSe(ns);
-	let mapp = await portReceiver(ns, "Server Map");
+	let mapp = await portReceiver(ns, "Server Map", 1,true);
 	//let tested = false;
 	for (let i in mapp) {
 		let content = ns.ls(mapp[i][0]);
