3445a8d9139a68896a58c73ed78a19b93b8957d4
Cerveau23 <cerveau23@gmail.com>
2026-01-22 19:13:52 +0100

    Re format

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,17 @@
-import { NS } from "@ns";
+import {NS} from "@ns";
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-  while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 1000, ns.args[2]) !== 0) { await ns.sleep(1); }
-  while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 100, ns.args[2]) !== 0) { await ns.sleep(1); }
-  while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 10, ns.args[2]) !== 0) { await ns.sleep(1); }
-  while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 1, ns.args[2]) !== 0) { await ns.sleep(1); }
+    while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 1000, ns.args[2]) !== 0) {
+        await ns.sleep(1);
+    }
+    while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 100, ns.args[2]) !== 0) {
+        await ns.sleep(1);
+    }
+    while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 10, ns.args[2]) !== 0) {
+        await ns.sleep(1);
+    }
+    while (ns.exec(ns.args[0] + ".js", ns.args[1] + "", 1, ns.args[2]) !== 0) {
+        await ns.sleep(1);
+    }
 }
