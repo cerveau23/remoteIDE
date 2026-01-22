cbf62103a0afa301d2019844c221d5122a72b020
Cerveau23 <cerveau23@gmail.com>
2026-01-22 13:57:32 +0100

    Fixed "Long unfocus upon server change seems to crash the game!"

--- src/semiAutoBackdoorerV2.ts
+++ src/semiAutoBackdoorerV2.ts
@@ -1,12 +1,9 @@
-// noinspection DuplicatedCode
-
 import {portReceiver} from "functions";
 import {dSe} from "depthScannerV2";
 import {keyPressAPI, serverPing, initialization as serverInitialization} from "BBA_API_handler"
 import {NS} from "@ns";
 import {ui} from "/functional/UIGetter"
 
-// noinspection JSUnusedLocalSymbols
 /** @param {NS} ns **/
 export async function main(ns: NS) {
 
@@ -15,12 +12,7 @@ export async function main(ns: NS) {
     // ---------------------------------
 
     await serverInitialization(ns, true);
-    /*    ns.tprint("Server initialised") */
-    /* ns.tail(); */
     let mapp = await portReceiver(ns, "Server Map", 1, true);
-    // ns.tprint(mapp.toString());
-    // ns.tprint(typeof mapp)
-    // ns.tprint(mapp[0])
     let serversWithoutBackdoors = [];
     let serversWithBackdoors = [];
     let others = [];
@@ -90,8 +82,9 @@ export async function main(ns: NS) {
                 if (await serverPing(true) && ui.isUserActive())
                     await autoEnter(ns);
             }
-        } else while ( ui.document.getElementById('terminal-input') !== undefined && ns.getServer(previousServer).isConnectedTo)
-            if(ui.isUserActive()) {
+        } else while ( ui.document.getElementById('terminal-input') !== undefined && ns.getServer(previousServer).isConnectedTo){
+            await ns.sleep(50);
+            if(ui.isUserActive())
                 await autoEnter(ns)
         }
         while (!ns.getServer(i).backdoorInstalled) {
@@ -104,9 +97,7 @@ export async function main(ns: NS) {
         // ns.run("beep.js", 1, 440); TODO: This generates a mega freeze. Let's fix it.
         previousServer = i;
     }
-    /* if (serversWithoutBackdoors.length === 0) { */
     ns.tprint("All servers backdoored!");
-    /* } */
     ns.write("lastHackingAtLevel.txt", ns.getHackingLevel().toString(), "w");
 }
 
@@ -124,126 +115,6 @@ export function copyToClipboard(text: string) {
         });
 }
 
-// noinspection JSUnusedLocalSymbols
-/**
- * @deprecated
- */
-function pasteFromClipboard() {
-    // Use the Clipboard API to read text from the clipboard
-    navigator.clipboard.readText()
-        .then(text => {
-            // Set the pasted text into the textarea
-            let s = <HTMLInputElement|null> document.getElementById('terminal-input');
-            if (s !== null) s.value = text;
-        })
-        .catch(err => {
-            // Handle any errors (e.g., permission denied)
-            console.error('Failed to read clipboard: ', err);
-        });
-}
-
-// noinspection JSUnusedLocalSymbols
-/**
- * @deprecated
- */
-function onSpace(ns: NS, event: { keyCode: number; }) {
-    //simulateKey("Space",32)
-    if (event.keyCode === 32) {
-        simulateKey(ns, "Enter", 13, true).then(() => {
-            return
-        });
-    }
-}
-
-// noinspection JSUnusedLocalSymbols
-/**
- * @param {String} keyToPress
- * @param {Number} keysCode
- */
-function simulateKeyV1(keyToPress: string, keysCode: number) {
-    // Create a new KeyboardEvent for the 'Enter' key
-    const enterKeyEvent = new KeyboardEvent('keydown', {
-        key: keyToPress,
-        keyCode: keysCode,
-        code: keyToPress,
-        which: keysCode,
-        //type: string,
-        metaKey: false,
-        altKey: false,
-        //cancelBubble: false,
-        cancelable: true,
-        charCode: 0,
-        //timeStamp: 3208093,
-        ctrlKey: false,
-        //currentTarget: null,
-        //defaultPrevented: true,
-        detail: 0,
-        //eventPhase: 0,
-        isComposing: false,
-        repeat: false,
-        //returnValue: false,
-        shiftKey: false,
-        bubbles: true,
-        //isTrusted: true,
-        composed: true
-    });
-    // Find the input element and dispatch the event
-    const inputElement = ui.document.getElementById('terminal-input');
-    inputElement?.dispatchEvent(enterKeyEvent);
-}
-
-/** @param {NS} ns
- * @param {String} keyToPress
- * @param {Number} keysCode
- * @param {boolean} upPress
- */
-export async function simulateKey(ns: NS, keyToPress: string, keysCode: number, upPress: boolean) {
-    let initDict = {
-        key: keyToPress,
-        keyCode: keysCode,
-        code: keyToPress,
-        which: keysCode,
-        type: "keydown",
-        metaKey: false,
-        altKey: false,
-        // cancelBubble: false,
-        cancelable: true,
-        charCode: 0,
-        // timeStamp: 3208093,
-        ctrlKey: false,
-        // currentTarget: null,
-        // defaultPrevented: true,
-        detail: 0,
-        // eventPhase: 0,
-        isComposing: false,
-        repeat: false,
-        // returnValue: false,
-        shiftKey: false,
-        bubbles: true,
-        // isTrusted: true,
-        composed: true
-    };
-    // Find the input element and dispatch the event
-    document.body.dispatchEvent(new KeyboardEvent('keydown', initDict));
-    initDict.type = "keypress";
-    document.body.dispatchEvent(new KeyboardEvent('keypress', initDict));
-    if (upPress) {
-        await ns.asleep(5);
-        initDict.type = "keyup";
-        document.body.dispatchEvent(new KeyboardEvent('keyup', initDict));
-    }
-}
-
-/*function triggerDivClick() {
-    // Select the div using its class name
-    const divElement = ui.document.querySelector('.MuiInputBase-root');
-    // Create a new click event
-    const clickEvent = new Event('click');
-    // Dispatch the click event on the div element
-    divElement?.dispatchEvent(clickEvent);
-}*/
-
-/* <div spellcheck="false" class="MuiInputBase-root MuiInput-root MuiInput-underline MuiInputBase-colorPrimary MuiInputBase-fullWidth MuiInputBase-formControl MuiInputBase-adornedStart css-1u3hywr-input"><p class="MuiTypography-root MuiTypography-body1 css-r3d8m1">[home&nbsp;/]&gt;&nbsp;</p><input aria-invalid="false" autocomplete="off" id="terminal-input" type="text" class="MuiInputBase-input MuiInput-input MuiInputBase-inputAdornedStart css-1oaunmp" value=""></div> */
 /**
  * @param {NS} ns
  * @param {Number} seconds
