4a48353d51861ac91e345cbef8c96dfafa668bc0
Cerveau23 <cerveau23@gmail.com>
2026-01-22 16:49:13 +0100

    Migrated functionforEvery.js to TS

--- src/functionforEvery.ts
+++ src/functionforEvery.ts
@@ -2,8 +2,9 @@
  * Adds two numbers together.
  * @param {Function} operation - The first number.
  * @param {any[]} array - The second number.
+ * @param {any[]} args
  */
-function forEvery(array, operation, ...args){
+function forEvery(array: any[], operation: Function, ...args: any[]){
     for (let i in array){
         operation (array[i],i,array, args)
     }
