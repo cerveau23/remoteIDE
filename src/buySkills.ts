3f3c9d510d26d5e419b49fea26dde9282645e0a9
Cerveau23 <cerveau23@gmail.com>
2026-01-26 14:53:25 +0100

    Fixed the algorythm by simply iterating in divisions by 4

--- src/buySkills.ts
+++ src/buySkills.ts
@@ -5,25 +5,26 @@ export async function main(ns: NS) {
     let target;
     do{
         target = await ns.prompt("What buy", {type: "select", choices: ns.bladeburner.getSkillNames()}) as false | BladeburnerSkillName
-    }while(typeof target === "boolean")
+    }while(target === false || target === undefined)
     let continuous = await ns.prompt("Permanent?",{type:"boolean"}) as boolean;
     ns.tprint("Target: " + target + " Permanent: " + continuous);
     ns.tprint(typeof target + typeof continuous);
     while(
         ns.bladeburner.getSkillUpgradeCost(target) < ns.bladeburner.getSkillPoints()
         || continuous){
-        if(ns.bladeburner.getSkillUpgradeCost(target) < ns.bladeburner.getSkillPoints()) {
-            ns.print("Points: " + ns.bladeburner.getSkillPoints())
-            ns.print("Skill cost:" + ns.bladeburner.getSkillUpgradeCost(target))
-            ns.print("Number: " + Math.floor(ns.bladeburner.getSkillPoints() / ns.bladeburner.getSkillUpgradeCost(target)))
-            ns.print(
-                ns.bladeburner.upgradeSkill(
-                    target,
-                    Math.floor(ns.bladeburner.getSkillPoints() / (ns.bladeburner.getSkillUpgradeCost(target) * 2)) + 1)
-                    ? "Upgraded " + target : "Failed to upgrade");
-        }
-        else{
-            ns.print("Not enough points")
+        for(let amount = ns.bladeburner.getSkillPoints(); amount > 0; ) {
+            if (ns.bladeburner.getSkillUpgradeCost(target, amount) < ns.bladeburner.getSkillPoints()) {
+                ns.print("Points: " + ns.bladeburner.getSkillPoints())
+                ns.print("Skill cost:" + ns.bladeburner.getSkillUpgradeCost(target))
+                ns.print("Number: " + amount)
+                ns.print(
+                    ns.bladeburner.upgradeSkill(target, amount)
+                        ? "Upgraded " + target : "Failed to upgrade");
+            } else {
+                ns.printf("Not enough points for %s levels", amount)
+                amount = Math.floor(amount / 4)
+            }
+            await ns.sleep(10 );
         }
         await ns.sleep(100);
     }
