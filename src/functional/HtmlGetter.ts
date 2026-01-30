34732d691539d4eb22a0d84bae9749547d1c08ef
Cerveau23 <cerveau23@gmail.com>
2026-01-30 22:34:04 +0100

    Had to add jsdoc to all overloads

--- src/functional/HtmlGetter.ts
+++ src/functional/HtmlGetter.ts
@@ -2,14 +2,35 @@ import { ui } from "./UIGetter";
 
 type functionCodes = "query"|"id"|"class"|"attribute"|"lastChild"
 
+/** <pre>
+ *           Entry | Return type
+ * ----------------|-----------------
+ * query/lastChild | Element
+ *           class | Element[]
+ *              id | HTMLElement
+ *       attribute | string </pre>
+ */
 type getHTMLReturnType<T extends functionCodes> =
     T extends "query"|"lastChild" ? Element :
         T extends "class" ? Element[] :
             T extends "id" ? HTMLElement :
                 T extends "attribute" ? string : null
-
-
+/**
+ * A function to get HTML elements/attributes without the risk of it being null.
+ * @template T among "query", "id", "class", "attribute" and "lastChild"
+ * @param {string} selector         The string to put in the selecting function.
+ * @param {T} desiredFunction       The type we're using to search.
+ * @param {HTMLElement} element     For the functions that search inside an element
+ * @returns {getHTMLReturnType<T>}
+ */
 export function getHTML<T extends "attribute"|"lastChild">(selector: string, desiredFunction: T , element: HTMLElement): getHTMLReturnType<T>
+/**
+ * A function to get HTML elements/attributes without the risk of it being null.
+ * @template T among "query", "id", "class", "attribute" and "lastChild"
+ * @param {string} selector         The string to put in the selecting function.
+ * @param {T} desiredFunction       The type we're using to search.
+ * @returns {getHTMLReturnType<T>}
+ */
 export function getHTML<T extends "query"|"id"|"class">(selector: string, desiredFunction: T): getHTMLReturnType<T>
 /**
  * A function to get HTML elements/attributes without the risk of it being null.
