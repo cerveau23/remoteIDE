afda9c9a4cac1c9d1258ca7958734d2bc991d196
Cerveau23 <cerveau23@gmail.com>
2026-01-31 11:54:32 +0100

    Created child classes for each RAM-costing source-file

--- src/functional/Source-File_State.ts
+++ src/functional/Source-File_State.ts
@@ -5,16 +5,9 @@ export class SourceFile_State_Lite {
     readonly corporation: boolean;
     readonly formulas: boolean;
     readonly gang: boolean;
-    readonly go: boolean;
-    readonly grafting: boolean;
-    readonly hacknet: boolean;
-    readonly singularity: boolean;
-    readonly sleeve: boolean;
     readonly stanek: boolean;
     readonly stock: boolean;
-    readonly advancedStock: boolean;
     constructor(ns: NS, desiredKnowledge : {[K in keyof SourceFile_State_Lite]?: boolean}) {
-        ns.ramOverride(1.6 + Number(desiredKnowledge.hacknet) * 4 + Number(desiredKnowledge.go) + Number(desiredKnowledge.grafting || desiredKnowledge.sleeve) + Number(desiredKnowledge.singularity) * 0.1)
         this.bladeburner =
             desiredKnowledge.bladeburner ?
                 "bladeburner" in ns &&
@@ -39,36 +32,6 @@ export class SourceFile_State_Lite {
                 typeof (ns as any).gang?.getBonusTime === "function"
                 : false;
 
-        this.go =
-            desiredKnowledge.go ?
-                "go" in ns &&
-                typeof (ns as any).go?.getCheatCount === "function" // Costs 1 GB
-                : false;
-
-        this.grafting =
-            desiredKnowledge.grafting || desiredKnowledge.sleeve ?
-                "grafting" in ns &&
-                typeof (ns as any).grafting?.waitForOngoingGrafting === "function" // Costs 1GB
-                : false;
-
-        this.hacknet =
-            desiredKnowledge.hacknet ?
-                "hacknet" in ns &&
-                typeof (ns as any).hacknet?.getCacheUpgradeCost === "function"
-                : false;// Costs 4GB
-
-        this.singularity =
-            desiredKnowledge.singularity ?
-                "singularity" in ns &&
-                typeof (ns as any).singularity?.isFocused === "function"
-                : false; // Costs 1.6 GB
-
-        this.sleeve =
-            desiredKnowledge.grafting || desiredKnowledge.sleeve ?
-                "grafting" in ns &&
-                typeof (ns as any).grafting?.waitForOngoingGrafting === "function" // Costs 1GB. Copy of Grafting since it's the same function and if there's one there's the other
-                : false;
-
         this.stanek =
             desiredKnowledge.stanek ?
                 "stanek" in ns &&
@@ -80,6 +43,76 @@ export class SourceFile_State_Lite {
                 "stock" in ns &&
                 typeof (ns as any).stock?.getBonusTime === "function"
                 : false;
+    }
+}
+
+export class SourceFile_State_Go extends SourceFile_State_Lite{
+    readonly go: boolean;
+    constructor(ns: NS, desiredKnowledge : {[K in keyof SourceFile_State_Go]?: boolean}) {
+        super(ns, desiredKnowledge);
+
+        this.go =
+            desiredKnowledge.go ?
+                "go" in ns &&
+                typeof (ns as any).go?.getCheatCount === "function" // Costs 1 GB
+                : false;
+    }
+
+}
+
+export class SourceFile_State_Grafting_N_Sleeve extends SourceFile_State_Lite{
+    readonly grafting: boolean;
+    readonly sleeve: boolean;
+    constructor(ns: NS, desiredKnowledge : {[K in keyof SourceFile_State_Grafting_N_Sleeve]?: boolean}) {
+        super(ns, desiredKnowledge);
+
+        this.grafting =
+            desiredKnowledge.grafting || desiredKnowledge.sleeve ?
+                "grafting" in ns &&
+                typeof (ns as any).grafting?.waitForOngoingGrafting === "function" // Costs 1GB
+                : false;
+
+        this.sleeve =
+            desiredKnowledge.grafting || desiredKnowledge.sleeve ?
+                "grafting" in ns &&
+                typeof (ns as any).grafting?.waitForOngoingGrafting === "function" // Costs 1GB. Copy of Grafting since it's the same function and if there's one there's the other
+                : false;
+    }
+
+}
+
+export class SourceFile_State_Hacknet extends SourceFile_State_Lite{
+    readonly hacknet: boolean;
+    constructor(ns: NS, desiredKnowledge : {[K in keyof SourceFile_State_Hacknet]?: boolean}) {
+        super(ns, desiredKnowledge);
+
+        this.hacknet =
+            desiredKnowledge.hacknet ?
+                "hacknet" in ns &&
+                typeof (ns as any).hacknet?.getCacheUpgradeCost === "function"
+                : false;// Costs 4GB
+    }
+
+}
+
+export class SourceFile_State_Singularity extends SourceFile_State_Lite{
+    readonly singularity: boolean;
+    constructor(ns: NS, desiredKnowledge : {[K in keyof SourceFile_State_Singularity]?: boolean}) {
+        super(ns, desiredKnowledge);
+
+        this.singularity =
+            desiredKnowledge.singularity ?
+                "singularity" in ns &&
+                typeof (ns as any).singularity?.isFocused === "function"
+                : false; // Costs 1.6 GB
+    }
+
+}
+
+export class SourceFile_State_Advanced_Stock extends SourceFile_State_Lite{
+    readonly advancedStock: boolean;
+    constructor(ns: NS, desiredKnowledge : {[K in keyof SourceFile_State_Advanced_Stock]?: boolean}) {
+        super(ns, desiredKnowledge);
 
         this.advancedStock =
             desiredKnowledge.advancedStock ?
@@ -87,4 +120,5 @@ export class SourceFile_State_Lite {
                 typeof (ns as any).stock?.buyShort === "function"
                 : false; // Costs 2.5 GB
     }
+
 }
